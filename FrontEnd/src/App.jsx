b8a9fc8d70505cd4907573332b82b2632a48b299
Yahya Afadisse <yahyaafadisse92@gmail.com>
2024-05-08 16:39:33 +0100

    add party rotage and modifier dans footer and nav

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,12 @@
 import './index.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Routage from './page/Routage';
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+      <Routage />
     </>
   );
 }
--- FrontEnd/src/page/Routage.jsx
+++ FrontEnd/src/page/Routage.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Layout from '../components/Layout'
+import Login from './Login'
+import Signup from './Signup'
+import Home from './Home'
+import Products from './Products'
+import Contact from './Contact'
 
 export default function Routage() {
   return (
-    <div>Routage</div>
+    <>
+        <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Layout />}>
+                <Route path='/home' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/signup' element={<Signup />}/>
+                <Route path='/products' element={<Products />}/>
+                <Route path='/contact' element={<Contact />}/>
+              </Route>
+           </Routes>
+         </BrowserRouter>
+        </>
   )
 }
