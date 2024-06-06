902de02f080af2cc644917716bdc1f8d86f19733
Yahya Afadisse <yahyaafadisse92@gmail.com>
2024-06-07 00:20:04 +0100

    delete B from admin

--- FrontEnd/src/admin/Sidebar.jsx
+++ FrontEnd/src/admin/Sidebar.jsx
@@ -12,8 +12,7 @@ function Sidebar() {
     return (
         <div className='bg-white sidebar p-2'>
         <div className='m-2'>
-            <i className='bi bi-bootstrap-fill me-3 fs-4'></i>
-            <span className='brand-name fs-4'>Admin</span>
+            <span className='brand-name fs-4'><b>Admin</b></span>
         </div>
         <hr className='text-dark' />
         <div className='list-group list-group-flush'>
